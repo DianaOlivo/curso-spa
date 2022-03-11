6d365dae018a5746af61f3338fc59954847d66e8
Diana Olivo <angelicaolivot@gmail.com>
2022-03-10 20:47:39 -0600

    proyecto SPA

---
+++ src/template/Header.js
@@ -0,0 +1,21 @@
+const Header = () =>{
+  const view = `
+    <div class="Header-main">
+      <div class="Header-logo">
+        <h1>
+          <a href="#">
+            100tifi.co
+          </a>
+        </h1>
+      </div>
+    </div>
+    <div class="HEader-nav">
+      <a href="#/about/">
+        About
+      </a>
+    </div>
+  `;
+  return view;
+};
+
+export default Header;
